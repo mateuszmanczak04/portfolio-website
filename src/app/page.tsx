df8b4a1ed4bb3c55b17e66b98c8504cd9c293bf2
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-01-02 10:14:06 +0100

    create About component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import About from '@/components/About';
 import Hero from '@/components/Hero';
 import Separator from '@/components/Separator';
 
 export default function Home() {
   return (
-    <div className='container pt-24 flex flex-col gap-16'>
+    <div className='container py-24 flex flex-col gap-16'>
       <Hero />
+      <Separator />
+      <About />
       <Separator />
     </div>
   );
---
+++ src/components/About.tsx
@@ -0,0 +1,17 @@
+const About = ({}) => {
+  return (
+    <div className='w-full flex flex-col gap-2'>
+      <h2 className='text-4xl font-bold'>O mnie</h2>
+      <p className='leading-7'>
+        Aktualnie jestem studentem I roku informatyki w Poznaniu, a po godzinach
+        szkolnych interesuję się programowaniem, szczególnie tworzeniem
+        aplikacji webowych. W celu zbudowania mojego portfolio stworzyłem tę
+        stronę jako wizytówkę i reklamę. Daję z siebie wszystko, by projekty,
+        które tworzę były na jak najwyższym poziomie i zadowoliły oko i portfel
+        klienta.
+      </p>
+    </div>
+  );
+};
+
+export default About;
