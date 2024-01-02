9d3eab7ced09852bbf3086c6f96e26331c6015de
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-01-02 10:11:05 +0100

    change tailwind container padding and body text color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import { Button } from '@/components/ui/button';
-import Image from 'next/image';
+import Hero from '@/components/Hero';
+import Separator from '@/components/Separator';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <Button variant='destructive'>Click me!</Button>
-      <Button variant='default'>Click me!</Button>
-      <Button variant='ghost'>Click me!</Button>
-      <Button variant='outline'>Click me!</Button>
-      <Button variant='link'>Click me!</Button>
-      <Button variant='secondary'>Click me!</Button>
+    <div className='container pt-24 flex flex-col gap-16'>
+      <Hero />
+      <Separator />
     </div>
   );
 }
