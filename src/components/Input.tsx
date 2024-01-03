1c4c607159bf4f9aeaa7b6fd9f4593f28d6addf9
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-01-03 11:17:08 +0100

    feat: add forwardRef to Input component

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from '@/lib/utils';
 import { type VariantProps, cva } from 'class-variance-authority';
-import { FC, createElement } from 'react';
+import { FC, createElement, forwardRef } from 'react';
 
 const inputVariants = cva(
   'flex h-10 w-full rounded-md border border-gray-200 bg-white px-3 py-2 text-sm placeholder:text-gray-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-primary disabled:cursor-not-allowed disabled:opacity-50',
@@ -31,26 +31,26 @@ export interface InputProps
   icon?: IconType;
 }
 
-export const Input: FC<InputProps> = ({
-  label,
-  className,
-  variant,
-  inputSize,
-  icon,
-  ...props
-}) => {
-  return (
-    <label className={cn('flex flex-col gap-1 w-full', className)}>
-      {label && <span>{label}</span>}
-      <div className='w-full relative'>
-        <input
-          className={cn(inputVariants({ variant, inputSize }), icon && 'pr-10')}
-          {...props}
-        />
-        <div className='absolute right-2 top-1/2 -translate-y-1/2'>
-          {icon && createElement(icon, { className: 'w-5 h-5' })}
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, className, variant, inputSize, icon, ...props }, ref) => {
+    return (
+      <label className={cn('flex flex-col gap-1 w-full', className)}>
+        {label && <span>{label}</span>}
+        <div className='w-full relative'>
+          <input
+            ref={ref}
+            className={cn(
+              inputVariants({ variant, inputSize }),
+              icon && 'pr-10'
+            )}
+            {...props}
+          />
+          <div className='absolute right-2 top-1/2 -translate-y-1/2'>
+            {icon && createElement(icon, { className: 'w-5 h-5' })}
+          </div>
         </div>
-      </div>
-    </label>
-  );
-};
+      </label>
+    );
+  }
+);
+Input.displayName = 'Input';
