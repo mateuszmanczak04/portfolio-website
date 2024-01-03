c3cea00db23a2821d95dd6af263c39bbdd6d396c
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-01-03 11:34:48 +0100

    feat: replace navigation links with buttons which scroll to sections
    rename navigation items

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -27,19 +27,19 @@ const Navigation = ({}) => {
           'fixed bg-white bottom-16 inset-x-0 flex flex-col justify-between border-t border-t-gray-200 gap-2 p-4 transition h-[320px] z-40',
           open ? 'translate-y-0' : 'translate-y-[384px]'
         )}>
-        <MobileNavigationItem href='#o-mnie' onClick={() => setOpen(false)}>
+        <MobileNavigationItem href='o-mnie' onClick={() => setOpen(false)}>
           O mnie
         </MobileNavigationItem>
-        <MobileNavigationItem href='#efekt' onClick={() => setOpen(false)}>
-          Efekt
+        <MobileNavigationItem href='efekt' onClick={() => setOpen(false)}>
+          Jaki efekt na ciebie czeka?
         </MobileNavigationItem>
-        <MobileNavigationItem href='#oferta' onClick={() => setOpen(false)}>
-          Oferta
+        <MobileNavigationItem href='oferta' onClick={() => setOpen(false)}>
+          Aktualna oferta
         </MobileNavigationItem>
-        <MobileNavigationItem href='#wspolpraca' onClick={() => setOpen(false)}>
-          Współpraca
+        <MobileNavigationItem href='wspolpraca' onClick={() => setOpen(false)}>
+          Przebieg współpracy
         </MobileNavigationItem>
-        <MobileNavigationItem href='#kontakt' onClick={() => setOpen(false)}>
+        <MobileNavigationItem href='kontakt' onClick={() => setOpen(false)}>
           Kontakt
         </MobileNavigationItem>
       </div>
@@ -61,12 +61,15 @@ const MobileNavigationItem: FC<{
   href: string;
 }> = ({ children, onClick, href }) => {
   return (
-    <Link
+    <Button
       className='flex-1 flex items-center justify-between w-full py-2 px-4 bg-gray-50 text-gray-800 hover:bg-gray-100 rounded-md cursor-pointer'
-      onClick={onClick}
-      href={href}>
+      onClick={() => {
+        const goToSection = document.getElementById(href);
+        if (goToSection) goToSection?.scrollIntoView({ behavior: 'smooth' });
+        onClick();
+      }}>
       {children}
       <ChevronRight />
-    </Link>
+    </Button>
   );
 };
