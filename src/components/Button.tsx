98afcd077e191cef603c198ddcac02cd4274b531
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-01-03 12:41:37 +0100

    feat: add icon to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,10 +2,10 @@
 
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
-import { FC, forwardRef } from 'react';
+import React, { FC, forwardRef } from 'react';
 
 export const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
+  'inline-flex items-center justify-center gap-1 whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50',
   {
     variants: {
       variant: {
@@ -26,18 +26,39 @@ export const buttonVariants = cva(
   }
 );
 
+const iconVariants = cva('w-4 h-4', {
+  variants: {
+    variant: {
+      default: 'text-white',
+      secondary: 'text-gray-800',
+    },
+  },
+  defaultVariants: {
+    variant: 'default',
+  },
+});
+
+type IconType = (props: { className: string }) => JSX.Element;
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {}
+    VariantProps<typeof buttonVariants> {
+  icon?: IconType;
+}
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, ...props }, ref) => {
+  ({ className, variant, size, icon, ...props }, ref) => {
     return (
       <button
         ref={ref}
         className={cn(buttonVariants({ variant, size, className }))}
-        {...props}
-      />
+        {...props}>
+        {icon &&
+          React.createElement(icon, {
+            className: iconVariants({ variant }),
+          })}
+        {props.children}
+      </button>
     );
   }
 );
