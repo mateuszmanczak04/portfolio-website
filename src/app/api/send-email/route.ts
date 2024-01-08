9f7fa64e7ed9bbcccaaad0a75dc5852d331aacbb
Mateusz Mańczak <mateuszmanczak16@gmail.com>
2024-01-08 19:59:10 +0100

    feat: attaching files to emails

--- src/app/api/send-email/route.ts
+++ src/app/api/send-email/route.ts
@@ -1,7 +1,34 @@
 import nodemailer from 'nodemailer';
+import { put } from '@vercel/blob';
 
 export async function POST(request: Request) {
-  const { email, name, message } = await request.json();
+  const formData = await request.formData();
+  let attachments: { path: string; filename: string }[] = [];
+
+  const files = formData.getAll('file');
+
+  // for (const file of files) instead of files.forEach()
+  // guarantees us awaiting every iteration
+  try {
+    for (const file of files) {
+      if (!(file instanceof File)) return;
+
+      // upload image to vercel blob storage
+      const { url } = await put(`articles/${file.name}`, file, {
+        access: 'public',
+      });
+      attachments.push({ path: url, filename: file.name });
+    }
+  } catch (err) {
+    console.log(err);
+    return Response.json(
+      {
+        message:
+          'Nie udało się wysłać wiadomości. Spróbuj ponownie później lub skorzystaj z innej formy kontaktu.',
+      },
+      { status: 500 }
+    );
+  }
 
   const transporter = nodemailer.createTransport({
     service: 'Gmail',
@@ -14,26 +41,34 @@ export async function POST(request: Request) {
     },
   });
 
-  const content = `${name}\n${email}\n\n${message}`;
+  // get values of form fields specified by the client
+  const email = formData.get('email');
+  const name = formData.get('name');
+  const content = formData.get('content');
+
+  const text = `${email}\n${name}\n\n${content}`;
 
   const mailOptions = {
     from: process.env.EMAIL,
     to: 'mateuszmanczak2004@gmail.com',
     subject: 'Wiadomość ze strony Portfolio',
-    text: content,
+    text,
+    attachments,
   };
 
-  transporter.sendMail(mailOptions, (error) => {
-    if (error) {
-      return Response.json(
-        { message: 'Pomyślnie wysłano wiadomość.' },
-        { status: 200 }
-      );
-    } else {
-      return Response.json(
-        { message: 'Nie udało się wysłać wiadomości. Spróbuj jeszcze raz.' },
-        { status: 500 }
-      );
-    }
-  });
+  try {
+    await transporter.sendMail(mailOptions);
+    return Response.json(
+      { message: 'Pomyślnie wysłano wiadomość.' },
+      { status: 200 }
+    );
+  } catch (err) {
+    return Response.json(
+      {
+        message:
+          'Nie udało się wysłać wiadomości. Spróbuj ponownie później lub skorzystaj z innej formy kontaktu.',
+      },
+      { status: 500 }
+    );
+  }
 }
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,27 +1,71 @@
 'use client';
 
-import { File, Mail, Send, User } from 'lucide-react';
+import { Mail, Send, User } from 'lucide-react';
 import { Input } from './Input';
 import { MultiLineInput } from './MultiLineInput';
-import { Button } from './Button';
-import { FormEvent } from 'react';
+import { Button, buttonVariants } from './Button';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import { FileInput } from './FileInput';
+import AttachmentPreview from './AttachmentPreview';
+import { cn } from '@/lib/utils';
 
 const ContactForm = () => {
+  const [responseMessage, setResponseMessage] = useState<string>('');
+  const [error, setError] = useState<string>('');
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [content, setContent] = useState<string>('');
+  const [files, setFiles] = useState<File[]>([]);
+
   const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
+    setLoading(true);
+    setResponseMessage('');
+    setError('');
 
-    const res = await fetch('/api/send-email', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        email: 'jankowalski1@gmail.com',
-        message: 'Witam, chciałbym zlecić stworzenie strony',
-        name: 'Jan Kowalski',
-      }),
-    });
+    const formData = new FormData();
+    if (files && files.length > 0) {
+      files.forEach((file) => {
+        formData.append('file', file);
+      });
+    }
+    formData.append('email', email);
+    formData.append('content', content);
+    formData.append('name', name);
+
+    try {
+      const res = await fetch('/api/send-email', {
+        method: 'POST',
+        body: formData,
+      });
+
+      if (res.ok) {
+        const data = await res.json();
+        setResponseMessage(data.message);
+        setEmail('');
+        setContent('');
+        setFiles([]);
+        setName('');
+      } else {
+        const data = await res.json();
+        throw new Error(data.message);
+      }
+    } catch (err: any) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSelectFile = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      setFiles((prev) => [
+        ...prev,
+        ...Array.prototype.slice.call(e.target.files), // convert FileList to array
+      ]);
+    }
   };
 
   return (
@@ -33,24 +77,75 @@ const ContactForm = () => {
         label='Imię i nazwisko'
         placeholder='Jan Kowalski'
         icon={(props) => <User {...props} />}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
       <Input
         label='E-mail'
+        type='email'
         placeholder='example@abc.com'
         icon={(props) => <Mail {...props} />}
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
       />
       <MultiLineInput
         label='Wiadomość'
         placeholder='Witam, chciałbym zlecić stworzenie bloga internetowego. Mój budżet to 2000 zł a termin wykonania to 2 tygodnie. Strona nie będzie skomplikowana. Proszę o pilny kontakt.'
         rows={8}
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
       />
-      <FileInput label='Załączniki' />
+      <div className='w-full flex flex-col gap-2'>
+        <label
+          className={buttonVariants({
+            variant: 'secondary',
+            className: 'cursor-pointer',
+          })}>
+          <FileInput
+            label='Załączniki'
+            onChange={handleSelectFile}
+            value=''
+            multiple
+            className='hidden'
+          />
+          Dodaj załączniki
+        </label>
+        {/* preview of images */}
+        <div
+          className={cn(
+            'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-2',
+            files.length < 1 && 'md:grid-cols-1'
+          )}>
+          {files.length < 1 && (
+            <p className='text-neutral-600 text-center w-full b1'>
+              Nie wybrano żadnych plików
+            </p>
+          )}
+          {files.map((file, index) => (
+            <AttachmentPreview
+              file={file}
+              key={index}
+              handleRemove={(fName: string) => {
+                setFiles((prev) => prev.filter((file) => file.name !== fName));
+              }}
+            />
+          ))}
+        </div>
+      </div>
       <Button
         type='submit'
         variant='default'
-        icon={(props) => <Send className={props.className} />}>
+        icon={(props) => <Send className={props.className} />}
+        disabled={!name || !email || !content || loading}>
         Wyślij wiadomość
       </Button>
+      <div className='w-full text-center'>
+        {loading && (
+          <p className='text-neutral-600'>Trwa wysyłanie wiadomości...</p>
+        )}
+        {responseMessage && <p className='text-success'>{responseMessage}</p>}
+        {error && <p className='text-error'>{error}</p>}
+      </div>
     </form>
   );
 };
@@ -62,5 +157,11 @@ const ContactForm = () => {
 // Jeśli nie, to tylko nazwę pliku
 // Wysyłasz je na serwer
 // Zostają dołączone do emaila
+
+// po powrocie: styl podglądu zdjęć
+// limit wielkosci plikow
+
+// TODO:
+// loading and error state
 
 export default ContactForm;
