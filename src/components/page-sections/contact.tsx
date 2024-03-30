3029f81ba7178d2a33e22a1d51758ecd1ab254c9
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-03-30 11:56:09 +0100

    change style of links in contact

--- src/components/page-sections/contact.tsx
+++ src/components/page-sections/contact.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import ContactForm from '@/components/form/contact-form';
+import { SquareArrowOutUpRight } from 'lucide-react';
 
 const Contact = () => {
 	return (
@@ -7,7 +8,7 @@ const Contact = () => {
 			className='mx-auto flex w-full max-w-screen-lg flex-col gap-4 px-4 py-16 md:py-20 lg:gap-8 lg:py-24'
 			id='kontakt'>
 			<h2 className='w-full text-center text-4xl font-bold'>Kontakt</h2>
-			<div className='flex flex-col gap-8 lg:flex-row lg:gap-6'>
+			<div className='flex flex-col gap-8 leading-7 lg:flex-row lg:gap-6'>
 				<ContactForm />
 				<div className='lg:flex-5 flex flex-col gap-4 px-2 lg:gap-6'>
 					<h3 className='text-2xl font-bold'>Inne formy kontaktu:</h3>
@@ -16,9 +17,10 @@ const Contact = () => {
 						<p className='break-all'>mateuszmanczak@icloud.com</p>
 						<Link
 							href='mailto:mateuszmanczak@icloud.com'
-							className='text-link hover:underline'
+							className='flex items-center gap-1 underline'
 							target='_blank'>
-							Napisz
+							<span>Napisz</span>
+							<SquareArrowOutUpRight className='h-4 w-4' />
 						</Link>
 					</div>
 					<div className='flex flex-col'>
@@ -26,42 +28,47 @@ const Contact = () => {
 						<p>+48 725 726 901</p>
 						<Link
 							href='tel:+48725726901'
-							className='text-link hover:underline'
+							className='flex items-center gap-1 underline'
 							target='_blank'>
-							Zadzwoń
+							<span>Zadzwoń</span>
+							<SquareArrowOutUpRight className='h-4 w-4' />
 						</Link>
 						<Link
 							href='sms:+48725726901'
-							className='text-link hover:underline'
+							className='flex items-center gap-1 underline'
 							target='_blank'>
-							Napisz SMS
+							<span>Napisz SMS</span>
+							<SquareArrowOutUpRight className='h-4 w-4' />
 						</Link>
 					</div>
 					<div className='flex flex-col'>
 						<p className='font-bold'>LinkedIn</p>
 						<Link
 							href='https://www.linkedin.com/in/mateusz-manczak/'
-							className='text-link hover:underline'
+							className='flex items-center gap-1 underline'
 							target='_blank'>
-							Przejdź do profilu
+							<span>Przejdź do profilu</span>
+							<SquareArrowOutUpRight className='h-4 w-4' />
 						</Link>
 					</div>
 					<div className='flex flex-col'>
 						<p className='font-bold'>Useme</p>
 						<Link
 							href='https://useme.com/pl/roles/contractor/mateusz-manczak,277291/'
-							className='text-link hover:underline'
+							className='flex items-center gap-1 underline'
 							target='_blank'>
-							Przejdź do profilu
+							<span>Przejdź do profilu</span>
+							<SquareArrowOutUpRight className='h-4 w-4' />
 						</Link>
 					</div>
 					<div className='flex flex-col'>
 						<p className='font-bold'>Fiverr</p>
 						<Link
 							href='https://www.fiverr.com/mateuszmanczak'
-							className='text-link hover:underline'
+							className='flex items-center gap-1 underline'
 							target='_blank'>
-							Przejdź do profilu
+							<span>Przejdź do profilu</span>
+							<SquareArrowOutUpRight className='h-4 w-4' />
 						</Link>
 					</div>
 				</div>
