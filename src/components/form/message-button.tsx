19d95b7d0ccd22a6e5ce1d2b6790c5230907f3bb
Mateusz Mańczak <mateuszmanczak@icloud.com>
2024-04-03 16:09:13 +0200

    fix double mt in message button

--- src/components/form/message-button.tsx
+++ src/components/form/message-button.tsx
@@ -15,7 +15,7 @@ const MessageButton = () => {
 	return (
 		<Button
 			size='lg'
-			className='mt-2 mt-4 flex w-full max-w-64 items-center gap-1'
+			className='mt-4 flex w-full max-w-64 items-center gap-1'
 			onClick={handleClick}>
 			<Send className='h-5 w-5' /> <span>Napisz wiadomość</span>
 		</Button>
